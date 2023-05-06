c66e72d505dd5af265c81479417dcf73a8f6e08e
Jesus Quintero <example@gmail.com>
2023-05-05 21:38:21 -0400

    Backend-chat

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const server = http.createServer();
+
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("Se ha conectado un cliente");
+
+  socket.broadcast.emit("chatMessage", {
+    user: "info",
+    message: "Se ha conectado un nuevo usuario",
+  });
+
+  socket.on("chatMessage", (data) => {
+      io.emit("chatMessage", data);
+  });
+});
+
+server.listen(3000);
